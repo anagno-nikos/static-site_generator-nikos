8973f6dec46e1de473d394403c50802d7184d46a
anagno-nikos <nikos.p.anagnostopoulos@gmail.com>
2025-06-04 01:10:31 +0300

    H: one of the final commits

--- src/copystatic.py
+++ src/copystatic.py
@@ -1,5 +1,7 @@
 import os, shutil
 
+from block_markdown import markdown_to_html_node
+
 def copy_source_destination(source = "", destination= ""):
         
     if not os.path.exists(source):
@@ -18,3 +20,51 @@ def copy_source_destination(source = "", destination= ""):
         else:
             copy_source_destination(  os.path.join(source, file) , os.path.join(destination, file) )
 
+
+def extract_title(markdown:str):
+    lines = markdown.split("\n")
+    for line in lines:
+        if not line.startswith("# "):
+            continue
+        return line.removeprefix("# ").strip()
+    raise Exception("no h1 header found")
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        from_path_content = f.read()
+    with open(template_path) as f:
+        template_html = f.read()
+    
+    from_html_string = markdown_to_html_node(from_path_content).to_html()
+    
+    title_md = extract_title(from_path_content)
+    
+    new_template_html = template_html.replace("{{ Title }}", title_md)
+    
+    html_to_write = new_template_html.replace("{{ Content }}", from_html_string)
+    
+    dest_dir = os.path.dirname(dest_path)
+    
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    
+    with open(dest_path, "x") as write_file:
+        write_file.write(html_to_write)
+        
+
+def generate_pages_recursive(
+    dir_path_content, 
+    template_path,
+    dest_dir_path
+):
+    
+    for dir in os.listdir(dir_path_content):
+        new_dir_content_path = os.path.join(dir_path_content, dir)
+        new_dir_dest_path = os.path.join(dest_dir_path, dir.replace(".md", ".html"))
+        if os.path.isdir(new_dir_content_path):
+            generate_pages_recursive(new_dir_content_path, template_path, new_dir_dest_path)
+        elif os.path.isfile(new_dir_content_path):
+            generate_page(new_dir_content_path, template_path, new_dir_dest_path)
+    
--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 import os
 import shutil
 
-from copystatic import copy_source_destination
+from copystatic import copy_source_destination, generate_page, generate_pages_recursive
 
-source_dir = "static"
-destination_dir = "public"
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
-    print("Deleting public directory")
-    if os.path.exists(destination_dir):
-        shutil.rmtree(destination_dir)
-    
-    print("Copying files from static to public directory...") 
-    copy_source_destination(source_dir, destination_dir)
-    
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_source_destination(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(
+        os.path.join(dir_path_content),
+        template_path,
+        os.path.join(dir_path_public),
+    )
+
+
 main()
